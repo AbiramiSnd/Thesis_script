51a88200c1743d57e30ac3ed40c7e708e4e8135b
AbiramiSnd <102949354+AbiramiSnd@users.noreply.github.com>
2024-10-18 10:02:43 +0200

    Update 3_Finding_RCA_supporting_mobilome_lr_reads_jan2024.sh

--- 3_Finding_RCA_supporting_mobilome_lr_reads_jan2024.sh
+++ 3_Finding_RCA_supporting_mobilome_lr_reads_jan2024.sh
@@ -4,13 +4,12 @@
 # Filter alignment for
 #	- reads atleast 200 pb length
 #	- MQ > 10
-#	- alignment length with read is atleast 100 pb long
 	
 # bedtools groupby is to group reads and count the number of time the same read is mapping to a locus
 # a unique ID is generated for each read mapping to a single locus by concatenating the columns about reads and mapping positions on reference genome : $1"_"$2"_"$3""$4"_"$5""$6"_"$7"_"$8,$1"_"$2"_"$3
 # Format the file to have the below output
 #
-awk -v OFS="\t" '(($3-$2)>=100 && $5>=200 && $9>=10) {print $1"_"$2"_"$3"_"$4"_"$5"_"$8,$6,$7,$7-$6,$8,$9}' 216MR_20_06.paf.bed  | bedtools groupby -g 1 -c 1,4,2,3,6 -o count,mean,collapse,collapse,mean  | sed 's/_/\t/g' | awk -v OFS="\t" '{print $1"_"$2"_"$3""$4"_"$5""$6"_"$7"_"$8,$1"_"$2"_"$3,$4,$5,$6,$7,$8,$9,$11,$12,$13}' > 216MR_20_06.paf.filt.txt
+awk -v OFS="\t" '($5>=200 && $9>=10) {print $1"_"$2"_"$3"_"$4"_"$5"_"$8,$6,$7,$7-$6,$8,$9}' 216MR_20_06.paf.bed  | bedtools groupby -g 1 -c 1,4,2,3,6 -o count,mean,collapse,collapse,mean  | sed 's/_/\t/g' | awk -v OFS="\t" '{print $1"_"$2"_"$3""$4"_"$5""$6"_"$7"_"$8,$1"_"$2"_"$3,$4,$5,$6,$7,$8,$9,$11,$12,$13}' > 216MR_20_06.paf.filt.txt
 
 # Custom python script is used to filter reads supporting RCA and is based on identifying reads mapping to the same locus multiple times
 python ../repeats_mobilome0.py 216MR_20_06.paf.filt.txt > 216MR_mobilome_minimap.paf_filt.bed.20_06.py_out
