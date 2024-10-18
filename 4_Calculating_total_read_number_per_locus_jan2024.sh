11d050c8440e8d7b2a7e82880dc2741a1acfcc20
AbiramiSnd <102949354+AbiramiSnd@users.noreply.github.com>
2024-10-18 10:04:46 +0200

    Update 4_Calculating_total_read_number_per_locus_jan2024.sh

--- 4_Calculating_total_read_number_per_locus_jan2024.sh
+++ 4_Calculating_total_read_number_per_locus_jan2024.sh
@@ -1,5 +1,8 @@
-# The aim of this script is to count the number of reads mapping per locus
+# The aim of this script is to count the number of reads mapping per locus and filter reads with repeats (having atleast 3 repeat units)
 
 # Selecting reads with MQ >=10
 # Merging reads alignment positions overlapping over 200 bp and count the number of reads mapping
-awk -v OFS="\t" '($9>=10) {print $0}' all_V25-001_mobilome.paf.bed | bedtools merge -d 200 -c 4,4 -o distinct,count_distinct | awk -v OFS="\t" '{print $1,$2,$3,$5}' | bedtools sort > all_V25-001_mobilome_minimap.paf.merged200_rd.bed
+awk -v OFS="\t" '($9>=10) {print $0}' 194MR_20_06.paf.bed | bedtools merge -d 200 -c 4,4 -o distinct,count_distinct | awk -v OFS="\t" '{print $1,$2,$3,$5}' | bedtools sort > all_194MR_mobilome.paf.merged200_rd.bed
+
+# Select reads having atleast 3 repeat units
+awk -v OFS="\t" '($6>2){print $0}' 194MR_mobilome_minimap_tmp.paf.py_out2 > 194MR_mobilome_minimap_tmp.paf.py_out3
