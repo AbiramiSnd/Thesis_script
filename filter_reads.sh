a0c078a893f3e25f4258aa5536ab3143c3056bbc
AbiramiSnd <102949354+AbiramiSnd@users.noreply.github.com>
2024-10-18 10:11:43 +0200

    Update filter_reads.sh

--- filter_reads.sh
+++ filter_reads.sh
@@ -1,14 +1,7 @@
-# Author : Abirami SOUNDIRAMOURTTY
-# PhD student, Laboratoire Génome et développement des plantes
-# abirami.soundiramourtty@univ-perp.fr 
-
-# The aim of this script is to filter raw ONT reads 
-# NanoFilt is required.
+# The aim of this script is to filter raw ONT WGS reads of #354M and #354R 
 
 # Select Pass reads
-
 awk '$1 ~ /pass/ {print $0; getline; print $0; getline;print $0;getline;print $0}' sample.fastq > sample_pass_reads.fastq 
 
 # Select reads with length >= 3kb
-
 NanoFilt -l 3000  sample_pass_reads.fastq >  sample_pass_reads.3kb.fastq
