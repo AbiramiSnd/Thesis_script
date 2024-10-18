dde4fc02c2ec69dd806e50aa658de4280655d07b
AbiramiSnd <102949354+AbiramiSnd@users.noreply.github.com>
2024-10-18 10:02:05 +0200

    Update 2_Mapping_mobilome_lr_ref_jan2024.sh

--- 2_Mapping_mobilome_lr_ref_jan2024.sh
+++ 2_Mapping_mobilome_lr_ref_jan2024.sh
@@ -1,4 +1,4 @@
-# The aim of this script is to map mobilome reads to reference genome
+# The aim of this script is to map ONT mobilome reads to reference genome and simplify the PAF file
 
 # Mapping mobilome nanopore reads to reference genome in paf format
 minimap2 -t 8 --cs -cx map-ont '/media/lgdp/TOSHIBA/Genome_ref_Fagus_v3/Fagus_sylvatica_v3.fasta' '/home/lgdp/Desktop/216MR/216MR.fastq' > 216MR_20_06.paf
