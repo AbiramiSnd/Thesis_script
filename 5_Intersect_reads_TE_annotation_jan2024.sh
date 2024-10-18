5079b7fb89975523d51241170f4bf02159866308
AbiramiSnd <102949354+AbiramiSnd@users.noreply.github.com>
2024-10-18 10:08:54 +0200

    Update 5_Intersect_reads_TE_annotation_jan2024.sh

--- 5_Intersect_reads_TE_annotation_jan2024.sh
+++ 5_Intersect_reads_TE_annotation_jan2024.sh
@@ -1,13 +1,12 @@
 # The aim of this script is to find per TE locus, the number of total read mapping and the number of reads supporting RCA
 
-# Intersecting 
+# Intersection
 #	- file with locus from reference genome with total number of reads
 #	- file with locus from reference genome with number of reads RCA supporting reads
-bedtools intersect -a all_V25-001_mobilome_minimap.paf.merged200_rd.bed -b all_V25-001_mobilome_minimap_tmp.paf.py_out2 -f 0.75 -loj  > all_V25-001_mobilome_minimap.paf.merged200_rd_all_V25-001_mobilome_minimap_tmp.paf.py_out2_loj.bed
-
+bedtools intersect -a all_194MR_mobilome.paf.merged200_rd.bed -b 194MR_mobilome_minimap_tmp.paf.py_out3 -f 0.7 -loj  > all_194MR_mobilome.paf.merged200_rd_all_194MR_mobilome_mobilome_minimap_tmp.paf.py_out3_loj.bed
 
 # Intersecting 
 #	- file with total reads/RCA supporting reads per locus 
 #	- filtered TE annotation
+bedtools intersect -a all_194MR_mobilome.paf.merged200_rd_all_194MR_mobilome_mobilome_minimap_tmp.paf.py_out3_loj.bed -b '/home/lgdp/Desktop/FsylCur4_refTEs_wclassif_wreliable.frags.no_microsat.bed' -wa -wb -f 0.2 | awk -v OFS="\t" '{print $0}' | sort |  uniq  | grep -v "Unclassified" > all_194MR_mobilome.paf.merged200_rd_all_194MR_mobilome_mobilome_minimap_tmp.paf.py_out3_loj-vs-TEs.bed
 
-bedtools intersect -a all_V25-001_mobilome_minimap.paf.merged200_rd_all_V25-001_mobilome_minimap_tmp.paf.py_out2_loj.bed -b '/home/lgdp/Desktop/FsylCur4_refTEs_wclassif_wreliable.frags.no_microsat.bed' -wa -wb -f 0.25 | awk -v OFS="\t" '{print $0}' | sort |  uniq  | grep -v "Unclassified" > all_V25-001_mobilome_minimap.paf.merged200_rd_all_V25-001_mobilome_minimap_tmp.paf.py_out2_loj-vs-TEs.bed
