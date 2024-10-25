5bf03b26e80457d2299b08fd5bd3c0f331b6c423
AbiramiSnd <102949354+AbiramiSnd@users.noreply.github.com>
2024-10-25 13:00:43 +0200

    Update all_cov_matrix.R

--- TIPs_detection/all_cov_matrix.R
+++ TIPs_detection/all_cov_matrix.R
@@ -16,21 +16,15 @@ args[0]
 args[1]
 args[2]
 
-
-
-# TIPs coverage matrix
-inputFile<-try(system(paste0("ls ",files,"/*all_sprite.regions.bed"),intern=TRUE)) #retro_lauren.regions.bed
-
-#inputFile<-try(system(paste0("ls ",files,"/*retro_lauren.regions.bed"),intern=TRUE)) 
+# Read all total local coverage matrix
+inputFile<-try(system(paste0("ls ",files,"/*all_sprite.regions.bed"),intern=TRUE)) 
 files <- unlist(strsplit(inputFile,split=" "))
 length(files)
 N<-data.frame()
 
+# Format numerical matrix of total coverage with all individuals and all windows
 N=read.table(paste0(args[2]),sep="\n",h=F)
-
-head(files)
 for (i in 1:length(files)){
-  
   res<-read.table(files[i],sep="\t",h=F)
   res$ID <- paste(res$V1,res$V2,res$V3,sep="_")
   tmp<-as.numeric(res$V4[match(N$V1, res$ID)]) 
@@ -43,7 +37,8 @@ pos<-paste0(args[2])
 POS<-read.table(pos,sep="\n",h=F)
 N2<-N[which(N$insertion %in% POS$V1),]
 
-colnames(N2) <- gsub(paste0("/home/lgdp/Desktop/TRACKPOSON_test/TRACKPOSON_test2/SPRITE/SPRITE1/cov_TIPs_2kb/"), '', colnames(N2), fixed=TRUE)
+colnames(N2) <- gsub(paste0("/home/lgdp/Desktop/TRACKPOSON_test/TRACKPOSON_test2/cov_all_windows/"), '', colnames(N2), fixed=TRUE)
 colnames(N2) <- gsub(paste0('.cov.all_sprite.regions.bed'), '', colnames(N2), fixed=TRUE)
-#colnames(N2) <- gsub(paste0('.cov.retro_lauren.regions.bed'), '', colnames(N2), fixed=TRUE)
+
+# Write matrix output
 write.csv(N2,args[3],quote=F,row.names = F,col.names = T)
