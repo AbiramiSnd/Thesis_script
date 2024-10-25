bf22761dda783c8b7f74406ccb09c62b794392a6
AbiramiSnd <102949354+AbiramiSnd@users.noreply.github.com>
2024-10-25 23:26:18 +0200

    Update and rename launch_GWAS_rmvp (1).sh to TIPs_gwas/launch_GWAS_rmvp.sh

--- TIPs_gwas/launch_GWAS_rmvp.sh
+++ TIPs_gwas/launch_GWAS_rmvp.sh
@@ -1,8 +1,7 @@
+# The aim of this script is to run GWAS for all matrices using a loop
 list=$(ls used_matrix2/*.csv)
-#list=$(ls 500_LTR_2.csv)
+
 for i in $list
 do
-
 ./Run_TE_gwas_Rmvp.sh $i
-#./Run_TE_gwas_Rmvp.sh $i
 done
