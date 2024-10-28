343f929112b9f79acc6aa4c565f36b035bda0d37
AbiramiSnd <102949354+AbiramiSnd@users.noreply.github.com>
2024-10-28 10:34:27 +0100

    Update and rename cov.sh to local_windows_coverage.sh

---
+++ TIPs_detection/local_windows_coverage.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#PBS -N bedmap_bam    
+#PBS -l nodes=1:ppn=6   
+#PBS -q long            
+#PBS -M abirami.soundiramourtty@univ-perp.fr  
+#PBS -m e          
+
+    
+# load tools
+source ~/.bashrc
+module load mosdepth/0.3.3
+
+# Variables
+
+FQ1=$file
+FQ2=$(echo $file | sed -e "s/_1/_2/")
+out=$(basename $file | awk -F "_" '{print $2}')
+
+DIR=/home/abirami/MASSANE
+bed=/home/abirami/MASSANE/all_500ltr_activeTE_ins.bed
+bam=/home/database/Beech_genomes/Massane/MAPPING_v3/BWA/$out-vs-Fagus_sylvatica_v3.sort.bam
+
+# Work in local
+mkdir /scratch/TRACKPOSON-$out
+cd  /scratch/TRACKPOSON-$out
+
+# Copy data in local directecoty
+cp $bed .
+ref=$(basename $bed)
+
+# Total read coverage by windows
+mosdepth --no-per-base --use-median --by $bed $out-all_10kb.cov $bam
+zcat $out-all_10kb.cov.regions.bed.gz > $out-all_10kb.cov.regions.bed
+
+# Cleaning temporary files
+cp $out-all_10kb.cov.regions.bed $DIR
+rm *mosdepth.*.txt
+rm *.gz.csi
+rm *.bed
+
+
+# Cleaning folder
+rm -r /scratch/TRACKPOSON-$out
